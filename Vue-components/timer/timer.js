5fb224bfac0ea5df046c05fbb61acb395a8897c6
Daniel Semler <47rooks@gmail.com>
2017-05-11 21:40:08 -0700

    Vue timer component

---
+++ Vue-components/timer/timer.js
@@ -0,0 +1,47 @@
+/*
+ * timer.js v0.1
+ * Copyright 47rooks.com 2017
+ * Release under MIT License.
+ *
+ * Timer requires Vue.js to be loaded before it.
+ */
+Vue.component('f7-timer', {
+  template: '<label>{{ displayString }}</label>',
+  data: function() {
+    return {
+      milliseconds: 0,
+      intervalTimer: null
+    }
+  },
+  computed: {
+    displayString: function() {
+        seconds = Math.floor(this.milliseconds / 1000);
+        minutes = Math.floor(this.milliseconds  / 60000);
+        hours = Math.floor(this.milliseconds / 3600000);
+        return ('0' + hours).slice(-2) + ":" +
+               ('0' + minutes % 60).slice(-2) + ":" +
+               ('0' + seconds % 60).slice(-2);
+      }
+  },
+  methods: {
+    start: function() {
+      var self = this;   // required for the timer function to see the
+                         //  data.milliseconds variable
+
+      if (this.intervalTimer) {
+        clearInterval(this.intervalTimer);
+      }
+      //this.milliseconds = Number(0);
+      this.intervalTimer = setInterval(function() {
+                                         self.$data.milliseconds += 1000
+                                       },
+                                       1000);
+    },
+    stop: function() {
+      console.log('stop called');
+      if (this.intervalTimer) {
+        clearInterval(this.intervalTimer);
+      }
+    }
+  }
+});
