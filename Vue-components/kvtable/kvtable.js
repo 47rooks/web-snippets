96d0c4b6e21da6935bc430a453e78b92210fdf6a
Daniel Semler <47rooks@gmail.com>
2017-07-14 20:05:35 -0700

    fix indentation

--- Vue-components/kvtable/kvtable.js
+++ Vue-components/kvtable/kvtable.js
@@ -5,28 +5,28 @@
  *
  * Timer requires Vue.js to be loaded before it.
  */
- Vue.component('f7-kvtable', {
-   template: `
-     <div>
-       <h3> {{heading}} </h3>
-       <table id="kvTable">
-         <tr v-for="elt in kv">
-           <td> {{elt.k}} </td>
-           <td class="number" v-if="!isNaN(parseFloat(elt.v)) && isFinite(elt.v)"> {{elt.v}} </td>
-           <td v-else> {{elt.v}} </td>
-         </tr>
-       </table>
-     </div>`,
-   data: function() {
-     return {
-       heading: null,
-       kv: [],
-     }
-   },
-   methods: {
-     set: function(kvData) {
-       this.heading = kvData.heading;
-       this.kv = kvData.kv;
-     }
-   }
- });
+Vue.component('f7-kvtable', {
+  template: `
+    <div>
+      <h3> {{heading}} </h3>
+      <table id="kvTable">
+        <tr v-for="elt in kv">
+          <td> {{elt.k}} </td>
+          <td class="number" v-if="!isNaN(parseFloat(elt.v)) && isFinite(elt.v)"> {{elt.v}} </td>
+          <td v-else> {{elt.v}} </td>
+        </tr>
+      </table>
+    </div>`,
+  data: function() {
+    return {
+      heading: null,
+      kv: [],
+    }
+  },
+  methods: {
+    set: function(kvData) {
+      this.heading = kvData.heading;
+      this.kv = kvData.kv;
+    }
+  }
+});
